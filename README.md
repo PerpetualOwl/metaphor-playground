b1184c9f09a1410e293d5af18a03448468de2762
PerpetualOwl <91967558+PerpetualOwl@users.noreply.github.com>
2023-10-02 03:08:19 +0000

    updated phrases and fixed requirement bugs

--- README.md
+++ README.md
@@ -2,6 +2,19 @@
 
 I made a quick query conversion script that makes some subset of queries work better in the metaphor api
 
+## setup
+
+python version 3.10.8
+
+install dependencies
+
+```pip install -r requirements.txt```
+
+download nlp model from spacy
+
+```python -m spacy download en_core_web_sm```
+
+
 ## usage
 
 ```python main.py <metaphor-api-key> <query>```
--- main.py
+++ main.py
@@ -4,14 +4,18 @@ import spacy
 import numpy as np
 from sklearn.metrics.pairwise import cosine_similarity
 
-# relatively lightweight model
 nlp = spacy.load("en_core_web_sm")
 
+import en_core_web_sm
+
+nlp = en_core_web_sm.load()
+
 reference_phrases = [
-    "Here are the best",
+    "Here are the",
     "Here is the",
     "I recommend",
-    "Top places to buy"
+    "Top places to",
+    "Worst things to"
 ]
 
 similarity_threshold = 0.7
@@ -22,27 +26,30 @@ def calculate_cosine_similarity(vec1, vec2):
 def convert_query(query):
     query_tokens = [token.text for token in nlp(query)]
     query_embeddings = np.array([token.vector for token in nlp(query)])
-    similarities = [calculate_cosine_similarity(query_embeddings, nlp(phrase).vector) for phrase in reference_phrases]
+    similarities = [calculate_cosine_similarity(query_embeddings[0], nlp(phrase).vector) for phrase in reference_phrases]
     max_similarity = max(similarities)
     
     if max_similarity < similarity_threshold:
-        converted_query = "Here are the" + " ".join(query_tokens)
+        converted_query = "Here are the " + " ".join(query_tokens)
         return converted_query
     else:
         return query
 
- if __name__ == "__main__":
-   if len(sys.argv) < 3:
-     print(f"Detected {len(sys.argv) - 1} arguments. Please use the following format: python main.py <metaphor api key> <query>")
-   metaphor = Metaphor(sys.argv[1])
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print(f"Detected {len(sys.argv) - 1} arguments. Please use the following format: python main.py <metaphor api key> <query>")
+    client = Metaphor(api_key=sys.argv[1])
 
-   query = sys.argv[2].lower()
+    query = " ".join(sys.argv[2:])
 
-   #hardcoded to make it faster
-   query = query.replace("what", "here")
+    #hardcoded to make it faster
+    query = query.lower().replace("what", "here")
 
-   converted_query = convert_query(query)
-   
-   results = metaphor.search(converted_query, use_autoprompt=True)
-   print(results)
+    converted_query = convert_query(query)
+
+    print(converted_query)
+
+    response = client.search(converted_query)
+
+    print(response)
    
